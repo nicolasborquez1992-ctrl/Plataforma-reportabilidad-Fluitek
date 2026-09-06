9cf9bc12124e70d05bde6537a6a4e4eaa0007831
nicolasborquez1992-ctrl <nicolas.borquez1992@gmail.com>
2026-09-06 13:04:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ fluitek_app_html = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fluitek - Sistema de Reportes Técnicos y Monitoreo</title>
+    <title>Fluitek - Sistema de Reportes Técnicos</title>
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- FontAwesome Icons -->
